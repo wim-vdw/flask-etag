4e0985ca60bba1ecd003dc7b3ef630fa86f76968
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2020-12-21 21:26:57 +0100

    add create feature

--- myapp/etag.py
+++ myapp/etag.py
@@ -1,8 +1,36 @@
-from flask import Blueprint
+from flask import Blueprint, request, make_response
+from datetime import datetime
+import hashlib
 
 etag_bp = Blueprint('etag', __name__)
+persons_database = {}
 
 
-@etag_bp.route('/')
-def index():
-    return 'Hello'
+@etag_bp.route('/persons', methods=['GET'])
+def persons_get():
+    pass
+
+
+@etag_bp.route('/persons/<person_id>', methods=['GET'])
+def person_get(person_id):
+    pass
+
+
+@etag_bp.route('/persons', methods=['POST'])
+def person_create():
+    data = request.get_json()
+    person_id = data.get('person_id')
+    person_name = data.get('person_name')
+    change_date = datetime.utcnow().isoformat()
+    etag = hashlib.md5(change_date.encode()).hexdigest()
+    response = make_response(dict(person_id=person_id,
+                                  person_name=person_name,
+                                  change_date=change_date,
+                                  etag=etag))
+    response.headers['ETag'] = etag
+    return response, 201
+
+
+@etag_bp.route('/persons/<person_id>', methods=['PUT'])
+def person_update(person_id):
+    pass
--- tests/test_etag.py
+++ tests/test_etag.py
@@ -1,4 +1,11 @@
-def test_fake(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.data == b'Hello'
+def test_person_create(client):
+    person_id = '1'
+    person_name = 'Wim'
+    response = client.post('/persons', json={person_id: person_id, person_name: person_name})
+    assert response.status_code == 201
+    data = response.get_json()
+    assert 'person_id' in data
+    assert 'person_name' in data
+    assert 'change_date' in data
+    assert 'etag' in data
+    assert 'ETag' in response.headers
